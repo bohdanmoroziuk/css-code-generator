171391936d55498a690b2a4d0ca3a76fa1e53cd3
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-27 16:36:26 +0200

    Update entry point

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+import rootReducer from './reducers/root';
 import App from './components/App';
 import ErrorBoundary from './service/ErrorBoundary';
 
@@ -10,12 +12,16 @@ import * as serviceWorker from './serviceWorker';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
+const store = createStore(rootReducer);
+
 const Index = () => (
-    <Router>
-        <ErrorBoundary>
-            <App />
-        </ErrorBoundary>
-    </Router>
+    <Provider store={store}>
+        <Router>
+            <ErrorBoundary>
+                <App />
+            </ErrorBoundary>
+        </Router>
+    </Provider>
 );
 
 render(
