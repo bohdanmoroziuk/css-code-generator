a6250fad62ae805a4777864abc9da9bf02cbc235
Bogdan Morozyuk <bogdan7morozyuk@gmail.com>
2019-01-28 09:26:32 +0200

    Updated component markup: NotFound

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,8 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => (
-    <div>
-        Page not found...
+    <div className="alert alert-warning" role="alert">
+        <h4 class="alert-heading">404</h4>
+        <hr />
+        <p>Page Not Found</p>
+        <Link className="btn btn-success" to="/">Home</Link>
     </div>
 );
 
